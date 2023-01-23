9362f0d7e9f0f15d521e8e47daa48209f403901d
Abdulla Abu Ainin <abdulla.ali.0098@gmail.com>
2023-01-23 22:57:54 +0100

    Black background

--- js/main.js
+++ js/main.js
@@ -17,34 +17,14 @@ function init() {
   camera.position.set( 100, 200, 300 );
 
   scene = new THREE.Scene();
-  scene.background = new THREE.Color( 0xa0a0a0 );
-  scene.fog = new THREE.Fog( 0xa0a0a0, 200, 1000 );
+  scene.background = new THREE.Color( 0x000000 );
+/*   scene.fog = new THREE.Fog( 0xa0a0a0, 200, 1000 ); */
 
   const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
   hemiLight.position.set( 0, 200, 0 );
   scene.add( hemiLight );
 
-  const dirLight = new THREE.DirectionalLight( 0xffffff );
-  dirLight.position.set( 0, 200, 100 );
-  dirLight.castShadow = true;
-  dirLight.shadow.camera.top = 180;
-  dirLight.shadow.camera.bottom = - 100;
-  dirLight.shadow.camera.left = - 120;
-  dirLight.shadow.camera.right = 120;
-  scene.add( dirLight );
-
   // scene.add( new THREE.CameraHelper( dirLight.shadow.camera ) );
-
-  // ground
-  const mesh = new THREE.Mesh( new THREE.PlaneGeometry( 2000, 2000 ), new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } ) );
-  mesh.rotation.x = - Math.PI / 2;
-  mesh.receiveShadow = true;
-  scene.add( mesh );
-
-  const grid = new THREE.GridHelper( 2000, 20, 0x000000, 0x000000 );
-  grid.material.opacity = 0.2;
-  grid.material.transparent = true;
-  scene.add( grid );
 
   // model
   const loader = new FBXLoader();
